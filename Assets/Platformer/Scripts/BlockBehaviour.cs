f6f1a22e1dc9232d6e5776373b434f7609e6c8a8
Justin Martinez <justinmnmj@gmail.com>
2025-02-18 12:33:24 -0800

    Testing Block Data

---
+++ Assets/Platformer/Scripts/BlockBehaviour.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class BlockBehaviour : MonoBehaviour
+{
+    public bool hasHit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        hasHit = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
